ff23f0427ae83681961a5a893e0ed80353431127
dcarjim0101 <dcarjim0101@g.educaand.es>
2025-05-29 11:21:06 +0200

    Update GestorPersistencia.java

--- ProyectoFinal/src/persistencia/GestorPersistencia.java
+++ ProyectoFinal/src/persistencia/GestorPersistencia.java
@@ -5,15 +5,16 @@ import modelo.Usuario;
 import java.io.*;
 
 public class GestorPersistencia {
-
+    //se establece ruta
     private static final String RUTA_ARCHIVO = "usuario.dat";
-
+    //metodo de guardar datos del usuario
     public static void guardarUsuario(Usuario usuario) throws IOException {
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(RUTA_ARCHIVO))) {
             oos.writeObject(usuario);
         }
     }
 
+    //comprueba que existe para leer o crear una nueva ademas de las excepciones
     public static Usuario cargarUsuario() throws IOException, ClassNotFoundException {
         File file = new File(RUTA_ARCHIVO);
         if (!file.exists()) {
